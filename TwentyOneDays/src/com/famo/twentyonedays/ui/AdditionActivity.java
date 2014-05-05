fad6243358743a571b09defe9315b0f781d8a3d9
lee <lee4755026@gmail.com>
2014-05-05 22:38:13 +0800

    添加wheelview 滚动时间选择控件

--- TwentyOneDays/src/com/famo/twentyonedays/ui/AdditionActivity.java
+++ TwentyOneDays/src/com/famo/twentyonedays/ui/AdditionActivity.java
@@ -4,7 +4,15 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import kankan.wheel.widget.OnWheelChangedListener;
+import kankan.wheel.widget.OnWheelClickedListener;
+import kankan.wheel.widget.OnWheelScrollListener;
+import kankan.wheel.widget.WheelView;
+import kankan.wheel.widget.adapters.NumericWheelAdapter;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
@@ -33,6 +41,11 @@ public class AdditionActivity extends Activity {
 	private View calendarView;
 	private TextView planDate;
 	private TextView planAlarmTime;
+	// Time changed flag
+	private boolean timeChanged = false;
+	
+	// Time scrolled flag
+	private boolean timeScrolled = false;
 	
 	SimpleDateFormat sdf=new SimpleDateFormat("yyyy/MM/dd");  
 
@@ -162,6 +175,98 @@ public class AdditionActivity extends Activity {
 			planDate.setText(start+"--"+end);
 					
 		}
+	}
+	
+	private void showDataPicker(){
+		View view=getLayoutInflater().inflate(R.layout.time_layout, null);
+		
+		final WheelView hours = (WheelView) view.findViewById(R.id.hour);
+		hours.setViewAdapter(new NumericWheelAdapter(this, 0, 23));
+	
+		final WheelView mins = (WheelView) view.findViewById(R.id.mins);
+		mins.setViewAdapter(new NumericWheelAdapter(this, 0, 59, "%02d"));
+		mins.setCyclic(true);
+	
+		final TimePicker picker = (TimePicker) view.findViewById(R.id.time);
+		picker.setIs24HourView(true);
+	
+		// set current time
+		Calendar c = Calendar.getInstance();
+		int curHours = c.get(Calendar.HOUR_OF_DAY);
+		int curMinutes = c.get(Calendar.MINUTE);
+	
+		hours.setCurrentItem(curHours);
+		mins.setCurrentItem(curMinutes);
+	
+		picker.setCurrentHour(curHours);
+		picker.setCurrentMinute(curMinutes);
+	
+		// add listeners
+		addChangingListener(mins, "min");
+		addChangingListener(hours, "hour");
+	
+		OnWheelChangedListener wheelListener = new OnWheelChangedListener() {
+			public void onChanged(WheelView wheel, int oldValue, int newValue) {
+				if (!timeScrolled) {
+					timeChanged = true;
+					picker.setCurrentHour(hours.getCurrentItem());
+					picker.setCurrentMinute(mins.getCurrentItem());
+					timeChanged = false;
+				}
+			}
+		};
+		hours.addChangingListener(wheelListener);
+		mins.addChangingListener(wheelListener);
+		
+		OnWheelClickedListener click = new OnWheelClickedListener() {
+            public void onItemClicked(WheelView wheel, int itemIndex) {
+                wheel.setCurrentItem(itemIndex, true);
+            }
+        };
+        hours.addClickingListener(click);
+        mins.addClickingListener(click);
+
+		OnWheelScrollListener scrollListener = new OnWheelScrollListener() {
+			public void onScrollingStarted(WheelView wheel) {
+				timeScrolled = true;
+			}
+			public void onScrollingFinished(WheelView wheel) {
+				timeScrolled = false;
+				timeChanged = true;
+				picker.setCurrentHour(hours.getCurrentItem());
+				picker.setCurrentMinute(mins.getCurrentItem());
+				timeChanged = false;
+			}
+		};
+		
+		hours.addScrollingListener(scrollListener);
+		mins.addScrollingListener(scrollListener);
+		
+		picker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+			public void onTimeChanged(TimePicker  view, int hourOfDay, int minute) {
+				if (!timeChanged) {
+					hours.setCurrentItem(hourOfDay, true);
+					mins.setCurrentItem(minute, true);
+				}
+			}
+		});
+		
+		AlertDialog.Builder builder=new AlertDialog.Builder(AdditionActivity.this)
+		.setView(view);
+		builder.create().show();
+	}
+
+	/**
+	 * Adds changing listener for wheel that updates the wheel label
+	 * @param wheel the wheel
+	 * @param label the wheel label
+	 */
+	private void addChangingListener(final WheelView wheel, final String label) {
+		wheel.addChangingListener(new OnWheelChangedListener() {
+			public void onChanged(WheelView wheel, int oldValue, int newValue) {
+				//wheel.setLabel(newValue != 1 ? label + "s" : label);
+			}
+		});
 	}
 
 }
